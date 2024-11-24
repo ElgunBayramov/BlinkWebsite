2a7d57affc278fd457432c0095a04244e60dfa44
Elguncode <elgunmb@code.edu.az>
2024-11-24 02:28:15 +0100

    first commit

---
+++ pages/contact/assets/js/contact.js
@@ -0,0 +1,45 @@
+// on load
+window.onload = function () {
+    window.setTimeout(load, 500);
+}
+
+function load() {
+    document.querySelector('.loader-wrapper').style.opacity = '0';
+    document.querySelector('.loader-wrapper').style.display = 'none';
+}
+// ---
+window.onscroll = function () {
+    
+    var scrollUp = document.querySelector(".scroll-top");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        scrollUp.style.display = "flex";
+    } else {
+        scrollUp.style.display = "none";
+    }
+    scrollUp.addEventListener("click",function(){
+        window.pageYOffset=0;
+    })
+};
+
+// hamburger open/close
+let navbarToggler = document.querySelector(".mobile-menu-btn");
+navbarToggler.addEventListener('click', function () {
+    navbarToggler.classList.toggle("active");
+});
+
+//  up/down arrow for hamburger dropdown
+let transforms=document.querySelectorAll("a.dd-menu");
+
+transforms.forEach(transform => {
+    transform.addEventListener("click",function(){
+        
+        if(!transform.classList.contains("collapsed")){
+
+            transform.style.setProperty('--transform','180deg')
+        }
+        if(transform.classList.contains("collapsed")){
+            transform.style.setProperty('--transform','0deg')
+        }
+        
+    })
+});
